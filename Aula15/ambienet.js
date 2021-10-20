3f6373e72cb35b205b8a9340a2380070d9431218
Bruno Milano <brn.milano@hotmail.com>
2021-10-20 13:58:23 -0300

    Ex 016

--- Aula15/ambienet.js
+++ Aula15/ambienet.js
@@ -12,4 +12,4 @@ console.log(`Agora, o meu vetor é o ${num}`);
 console.log(num.length);
 
 let pos = num.indexOf(3)
-console.log(`O valor 3 está dentro da posição ${pos}`);
+console.log(`O valor 3 está dentro da posição ${pos}`)
---
+++ Aula16/funcao01.js
@@ -0,0 +1,10 @@
+function parimp(n) {
+  if (n % 2 == 0) {
+    return 'par'
+  } else {
+    return "ímpar"
+  }
+}
+
+let res = parimp(10)
+console.log(res);
